9cced8364a3c82f8bedd04fab0ecc20effd609d6
Nguyen Anh <cauut2117610@gmail.com>
2015-03-24 08:12:15 +0700

    update autocomplete

--- public/javascripts/autocomplete.js
+++ public/javascripts/autocomplete.js
@@ -1,30 +1,54 @@
+var projects = [
+      {
+        value: "jquery",
+        label: "jQuery",
+        desc: "the write less, do more, JavaScript library",
+        icon: "jquery_32x32.png"
+      },
+      {
+        value: "jquery-ui",
+        label: "jQuery UI",
+        desc: "the official user interface library for jQuery",
+        icon: "jqueryui_32x32.png"
+      },
+      {
+        value: "sizzlejs",
+        label: "Sizzle JS",
+        desc: "a pure-JavaScript CSS selector engine",
+        icon: "sizzlejs_32x32.png"
+      }
+    ];
 $("#autocomplete").autocomplete({
+  // source: projects,
   source: function(request, response){
     var name = $('#autocomplete').val();
-    console.log(name);
     $.ajax({
       type: "POST",
       url: "http://localhost:3000/user/ajax_search",
       data: {username: name},
-      // contentType: "application/json; charset=utf-8",
-      // dataType: "json",
       success: function (msg) {
-        console.log(msg);
-        // response($.parseJSON(msg.d).Records);
+        // console.log(JSON.stringify(msg));
+        // response(JSON.stringify(msg));
+        response(msg);
       },
       error: function (msg) {
          alert(msg.status + ' ' + msg.statusText);
       }
     });
    },
-   // select: function (event, ui) {
-   //     $("#WorkItem").val(ui.item.Work_Item);
-   //     return false;
-   // }
-});
-// .data("autocomplete")._renderItem = function (ul, item) {
-//     return $("<li></li>")
-//     .data("item.autocomplete", item)
-//     .append("<a>" + item.Work_Item + "</a>")
-//     .appendTo(ul);
-// };
+  focus: function( event, ui ) {
+    $( "#autocomplete" ).val( ui.item.username  );
+    return false;
+  },
+  select: function( event, ui ) {
+    $( "#autocomplete" ).val( ui.item.username );
+    $( "#user_id" ).val( ui.item._id  );
+    $( "#user_username" ).html( ui.item.desc );
+    return false;
+  }
+})
+.autocomplete( "instance" )._renderItem = function( ul, item ) {      
+  return $("<li>")
+        .append("<a>"+item.firstname+"("+item.username+")</a>" )
+        .appendTo(ul);
+    };
